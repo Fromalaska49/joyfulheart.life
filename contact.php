1f93c9928183ec4affc835d5ae60627002d3da19
Andrew Hutton <fromalaska49@gmail.com>
2016-12-26 21:47:08 -0800

    Cloning Existing Project

---
+++ contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			require('common/head-includes.php');
+		?>
+        <title>Contact Us | Association for Computing Machinery UTSA</title>
+	</head>
+	<body>
+		<?php
+			require('common/menu.php');
+		?>
+		<div id="content" class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row section-container group">
+						<div class="col-sm-12">
+							<h1>Contact Us</h1>
+							<br />
+							<div class="row">
+								<div class="col-sm-8">
+									<p>
+										ACM's primary form of communication is through <a href="index#slack-container">Slack</a> at <a href="http://acm-utsa.slack.com">acm-utsa.slack.com</a>
+									</p>
+								</div>
+								<div class="col-sm-4" style="text-align:center;">
+									<a href="http://acm-utsa.slack.com">
+										<input type="image" src="images/icons/add-to-slack.png" class="slack-button"/>
+									</a>
+								</div>
+							</div>
+							<br />
+							<p>
+								For any general inquiries, comments, questions, or any other business, please
+								contact us at <a href="mailto:acm.utsa@gmail.com">acm.utsa@gmail.com</a>.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php
+			require('common/footer.php');
+		?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			require('common/head-includes.php');
+		?>
+        <title>The Association for Computing Machinery &mdash; UTSA</title>
+	</head>
+	<body>
+		<?php
+			require('common/menu.php');
+		?>
+		<div id="content" class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row section-container">
+						<div class="col-sm-12">
+							Interesting stuff here..
+						</div>
+					</div>
+					<div class="row section-container">
+						Friday
+						<br />
+						January 13, 2016
+					</div>
+					<!--
+						<div class="row section-container" style="display:table;" id="slack-container">
+							<div class="col-sm-8">
+								<h1>
+									Slack
+								</h1>
+								<p>
+									Want to share notes from class? Want to hear about events on campus or talk about the latest and greatest stuff in technology? From app development to unix security, ACM has people who share your interests. The best way to get connected with other students and hear about upcoming events, is through <a href="http://acm-utsa.slack.com">Slack</a>. We use Slack to share information about internships, events, and opportunities at UTSA and around town. Members can recieve an invitation to join by sending an email to <a href="mailto:acm.utsa@gmail.com">acm.utsa@gmail.com</a> or by contacting one of our <a href="officers.php">officers</a>.
+								</p>
+							</div>
+							<div class="col-sm-4 hidden-xs">
+								<div style="margin:120px 0px 0px 0px;text-align: center;">
+									<a href="http://acm-utsa.slack.com">
+										<input type="image" src="images/icons/add-to-slack.png" class="slack-button"/>
+									</a>
+								</div>
+							</div>
+							<div class="col-sm-4 hidden-sm hidden-md hidden-lg hidden-xl">
+								<div style="margin:40px 0px 0px 0px;text-align: center;">
+									<a href="http://acm-utsa.slack.com">
+										<input type="image" src="images/icons/add-to-slack.png" class="slack-button"/>
+									</a>
+								</div>
+							</div>
+						</div>
+					-->
+				</div>
+			</div>
+		</div>
+		<?php
+			require('common/footer.php');
+		?>
+	</body>
+</html>
